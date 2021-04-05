7099e45462e5f8a0966daaaa8cfb5d9ac6d61869
Harsh Madhani <harshmadhani@gmail.com>
2021-04-05 16:05:40 +0530

    Adding github oauth2 authentication

--- src/main/java/com/harshmadhani/resource/JiraIssueResource.java
+++ src/main/java/com/harshmadhani/resource/JiraIssueResource.java
@@ -5,13 +5,20 @@ import com.harshmadhani.model.JiraIssueSearch;
 import com.harshmadhani.service.JiraIssueService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
 
 @Path("/jira")
+@ApplicationScoped
 public class JiraIssueResource {
     @Inject
     @RestClient
@@ -19,13 +26,21 @@ public class JiraIssueResource {
 
     @GET
     @Path("/issue/{issueNumber}")
-    public JiraIssue getIssue(@PathParam("issueNumber") String issueNumber){
+    @PermitAll
+    public JiraIssue getIssue(@PathParam("issueNumber") String issueNumber, @Context SecurityContext ctx){
+        Principal caller = ctx.getUserPrincipal();
+        String name = caller == null ? "anonymous" : caller.getName();
+        System.out.println("hello "+name+" issecure "+ctx.isSecure()+" authscheme "+ctx.getAuthenticationScheme());
         return jiraIssueService.getJiraIssueByNumber(issueNumber);
     }
 
     @GET
     @Path("/searchissue")
-    public JiraIssueSearch getIssuesByJql(@QueryParam("jql") String jql){
+    @RolesAllowed({"Subscriber","Echoer"})
+    public JiraIssueSearch getIssuesByJql(@QueryParam("jql") String jql, @Context SecurityContext ctx){
+        Principal caller = ctx.getUserPrincipal();
+        String name = caller == null ? "anonymous" : caller.getName();
+        System.out.println("hello "+name+" issecure "+ctx.isSecure()+" authscheme "+ctx.getAuthenticationScheme());
         return jiraIssueService.getJiraIssuesByJql(jql);
     }
 }
