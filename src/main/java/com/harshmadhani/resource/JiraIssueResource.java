c09c7c975411f41d6c9b90d964b311b007aa7868
Harsh Madhani <harshmadhani@gmail.com>
2021-04-05 18:18:46 +0530

    Revert "Adding github oauth2 authentication"
    This reverts commit 7099e454

--- src/main/java/com/harshmadhani/resource/JiraIssueResource.java
+++ src/main/java/com/harshmadhani/resource/JiraIssueResource.java
@@ -5,20 +5,13 @@ import com.harshmadhani.model.JiraIssueSearch;
 import com.harshmadhani.service.JiraIssueService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.SecurityContext;
-import java.security.Principal;
 
 @Path("/jira")
-@ApplicationScoped
 public class JiraIssueResource {
     @Inject
     @RestClient
@@ -26,21 +19,13 @@ public class JiraIssueResource {
 
     @GET
     @Path("/issue/{issueNumber}")
-    @PermitAll
-    public JiraIssue getIssue(@PathParam("issueNumber") String issueNumber, @Context SecurityContext ctx){
-        Principal caller = ctx.getUserPrincipal();
-        String name = caller == null ? "anonymous" : caller.getName();
-        System.out.println("hello "+name+" issecure "+ctx.isSecure()+" authscheme "+ctx.getAuthenticationScheme());
+    public JiraIssue getIssue(@PathParam("issueNumber") String issueNumber){
         return jiraIssueService.getJiraIssueByNumber(issueNumber);
     }
 
     @GET
     @Path("/searchissue")
-    @RolesAllowed({"Subscriber","Echoer"})
-    public JiraIssueSearch getIssuesByJql(@QueryParam("jql") String jql, @Context SecurityContext ctx){
-        Principal caller = ctx.getUserPrincipal();
-        String name = caller == null ? "anonymous" : caller.getName();
-        System.out.println("hello "+name+" issecure "+ctx.isSecure()+" authscheme "+ctx.getAuthenticationScheme());
+    public JiraIssueSearch getIssuesByJql(@QueryParam("jql") String jql){
         return jiraIssueService.getJiraIssuesByJql(jql);
     }
 }
