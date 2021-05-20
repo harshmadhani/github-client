5c6db238fbf3079780a3edcbdbcecc4b00dff560
Harsh Madhani <harshmadhani@gmail.com>
2021-05-20 21:46:10 +0530

    Adding logic to check for a specific label and added logging to the classes

--- src/main/java/com/harshmadhani/model/GithubPullRequest.java
+++ src/main/java/com/harshmadhani/model/GithubPullRequest.java
@@ -2,12 +2,18 @@ package com.harshmadhani.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GithubPullRequest {
     public String number;
     public String state;
     public Milestone milestone;
+    public Label[] labels;
     public static class Milestone{
         public String title;
+    }
+    public static class Label{
+        public String name;
     }
 }
--- src/main/java/com/harshmadhani/resource/GithubJiraResource.java
+++ src/main/java/com/harshmadhani/resource/GithubJiraResource.java
@@ -5,10 +5,12 @@ import com.harshmadhani.model.JiraIssue;
 import com.harshmadhani.service.GithubPullRequestService;
 import com.harshmadhani.service.JiraIssueService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,16 +28,18 @@ public class GithubJiraResource {
     @RestClient
     GithubPullRequestService githubPullRequestService;
 
+    private static final Logger log = Logger.getLogger(GithubJiraResource.class);
+
     @GET
     @Path("/invalidjira")
-    public List<JiraIssue> getInvalidJira(){
+    public List<JiraIssue> getInvalidJira(@QueryParam("label") String label){
         List<JiraIssue> invalidJiras = new ArrayList<>();
         String jql = "project=QUARKUS AND fixVersion=1.11.7.GA AND component=\"team/eng\" AND status = Implemented";
         JiraIssue[] jiras = jiraIssueService.getJiraIssuesByJql(jql).jiras;
         if(jiras.length>0){
             List<JiraIssue> jiraList = Arrays.asList(jiras);
             jiraList.stream().forEach(jira->{
-                if(!checkMilestone(jira))
+                if(!checkLabelAndMilestone(jira,label))
                     invalidJiras.add(jira);
             });
             return invalidJiras;
@@ -43,15 +47,17 @@ public class GithubJiraResource {
         return Collections.emptyList();
     }
 
-    private boolean checkMilestone(JiraIssue jira) {
+    private boolean checkLabelAndMilestone(JiraIssue jira, String labelToCheck) {
         String[] prList = jira.fields.github_pr;
         if(prList !=null && prList.length>0){
             String prNumber = Stream.of(prList[0].split("\\/")).reduce((first, last)->last).get();
-            System.out.println(prNumber);
+            log.debug("PR number is "+prNumber);
             GithubPullRequest pullRequest = githubPullRequestService.getPullRequestByNumber(prNumber);
-            if(pullRequest!=null && pullRequest.milestone!=null) {
-                System.out.println(pullRequest.state);
-                System.out.println(pullRequest.milestone.title);
+            if(pullRequest != null
+                    || (pullRequest.labels != null && Stream.of(pullRequest.labels).anyMatch(label->label.name.equals(labelToCheck)))
+                    || (pullRequest.milestone!= null)){
+                log.debug("state of PR is "+pullRequest.state);
+                log.debug("milestone of PR is "+pullRequest.milestone.title);
                 return true;
             }
             return false;
